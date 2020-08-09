ec6f8e47716576ad80bd9ad6e5ed01f3da81ae92
unknown <hannzaimonn@gmail.com>
2020-08-09 22:10:41 +0900

    レイアウト追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -43,7 +43,7 @@ class UsersController < ApplicationController
 
   private
   def user_params
-  	params.require(:user).permit(:name, :introduction, :image)
+  	params.require(:user).permit(:name, :body, :image)
   end
 
   #url直接防止
