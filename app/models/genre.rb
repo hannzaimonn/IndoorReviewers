e39f8d8a788b1b45a05328ce712ad9b586a704bb
unknown <hannzaimonn@gmail.com>
2020-09-05 03:00:02 +0900

    微修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -2,5 +2,5 @@ class Genre < ApplicationRecord
 	has_many :reviews
 	belongs_to :user
 
-	validates :name, presence: true, confirmation: true
+	validates :name, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i}
 end
